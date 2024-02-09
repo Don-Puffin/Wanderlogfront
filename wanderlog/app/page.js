2044742629044c3277b14fea1f756193c02f3b38
phianova <98988580+phianova@users.noreply.github.com>
2024-02-09 12:35:49 +0000

    register page and feed component card

--- wanderlog/app/page.js
+++ wanderlog/app/page.js
@@ -1,3 +1,16 @@
 //Landing page
 
 //If already logged in, route to feed
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+    </div>
+  )
+}
+
+export default page
